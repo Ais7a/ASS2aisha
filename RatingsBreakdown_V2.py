ab0b94678b9e26a923e12eea78dcf43ac7c804fd
Aisha <114300666+Ais7a@users.noreply.github.com>
2022-11-30 17:52:44 +0300

    Add files via upload

---
+++ RatingsBreakdown_V2.py
@@ -0,0 +1,28 @@
+# %%file is an Ipython magic function that saves the code cell as a file
+
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class RatingsBreakdown_V2(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_ratings,
+                   reducer=self.reducer_count_ratings)
+        ]
+
+    def mapper_get_ratings(self, _, line):
+        (userID, movieID, rating, timestamp) = line.split('\t')
+        yield movieID,1
+        
+        
+    def mapper_get_ratings_1(self, _, line):
+        (userID, movieID, rating, timestamp) = line.split('\t')
+        yield rating,1
+                
+
+    def reducer_count_ratings(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    RatingsBreakdown_V2.run()
+
